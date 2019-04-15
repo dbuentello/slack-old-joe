f9e1e202e34219f2b7ef07ee2005e1827d32dd12
Felix Rieseberg <felix@felixrieseberg.com>
2019-04-15 11:09:36 -0700

    chore: Handle macOS paths

--- src/helpers/get-slack-path.ts
+++ src/helpers/get-slack-path.ts
@@ -2,19 +2,47 @@ import * as path from 'path';
 import * as fs from 'fs-extra';
 import * as os from 'os';
 
-export async function getSlackPath(version = '') {
+export async function getSlackPath(version: string): Promise<string | null> {
+  let expectedPath;
+  let expectedDir;
+
   if (process.platform === 'win32') {
-    const homedir = os.homedir();
-    const slackDir = path.join(homedir, `AppData/Local/slack`);
-    const slackExe = path.join(slackDir, `app-${version}/slack.exe`);
+    ({ expectedDir, expectedPath } = await getSlackPathWindows(version));
+  }
 
-    if (fs.existsSync(slackExe)) {
-      return slackExe;
-    } else {
-      const contents = `\n - ` + (await fs.readdir(slackDir)).join(`\n - `);
-      const warning = `Could not find Slack for v${version}. Here is what we have: ${contents}`;
+  if (process.platform === 'darwin') {
+    ({ expectedDir, expectedPath } = await getSlackPathMac());
+  }
 
-      console.log(warning);
-    }
+  if (fs.existsSync(expectedPath)) {
+    return expectedPath;
+  } else {
+    const contents = `\n - ` + (await fs.readdir(expectedDir)).join(`\n - `);
+    const warning = `Could not find Slack for v${version}. Here is what we have: ${contents}`;
+
+    console.log(warning);
+  }
+
+  return null;
+}
+
+async function getSlackPathWindows(version: string) {
+  const homedir = os.homedir();
+  const slackDir = path.join(homedir, `AppData/Local/slack`);
+  const slackExe = path.join(slackDir, `app-${version}/slack.exe`);
+
+  return {
+    expectedDir: slackDir,
+    expectedPath: slackExe
+  }
+};
+
+async function getSlackPathMac() {
+  const slackDir = path.join(`/Applications/Slack.app`);
+  const slackBin = path.join(slackDir, `Contents/MacOS/Slack`);
+
+  return {
+    expectedDir: slackDir,
+    expectedPath: slackBin
   }
 }
