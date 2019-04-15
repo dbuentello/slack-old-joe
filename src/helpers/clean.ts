0812011ea205f45a1e1fe600a786166255d00a65
Felix Rieseberg <felix@felixrieseberg.com>
2019-04-15 12:02:53 -0700

    feat: Add clean helper

--- src/helpers/clean.ts
+++ src/helpers/clean.ts
@@ -3,8 +3,32 @@ import * as fs from 'fs-extra';
 import * as os from 'os';
 
 export async function clean() {
+  let pathToClean: string;
+
+  if (process.platform === 'win32') {
+    pathToClean = getWindowsDir();
+  } else if (process.platform === 'darwin') {
+    pathToClean = getDarwinDir();
+  } else {
+    throw new Error(`Platform ${process.platform} not implemented yet!`);
+  }
+
+  await fs.emptyDir(pathToClean);
+}
+
+function getWindowsDir() {
   const homedir = os.homedir();
   const slackDir = path.join(homedir, `AppData/Roaming/SlackDevMode`);
 
-  await fs.emptyDir(slackDir);
+  return slackDir;
+}
+
+function getDarwinDir() {
+  const homedir = os.homedir();
+  const slackDir = path.join(
+    homedir,
+    `~/Library/Application Support/SlackDevMode`
+  );
+
+  return slackDir;
 }
