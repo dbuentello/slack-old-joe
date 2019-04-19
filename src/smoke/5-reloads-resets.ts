803060a855f6a09034ba90b5f5d90d248d07d46b
Felix Rieseberg <felix@felixrieseberg.com>
2019-04-19 12:43:44 -0700

    feat: Test reload

---
+++ src/smoke/5-reloads-resets.ts
@@ -0,0 +1,29 @@
+import * as assert from 'assert';
+
+import { SuiteMethod } from '../interfaces';
+import { wait } from '../helpers/wait';
+import { getBrowserViewHandle } from '../helpers/get-browser-view';
+import { reload } from '../native-commands/reload';
+
+export const test: SuiteMethod = async (
+  client,
+  { it, beforeAll, afterAll, beforeEach, afterEach }
+) => {
+  beforeAll(async () => {
+    await getBrowserViewHandle(client);
+  });
+
+  it('can reload a workspace', async () => {
+    // Leave a breadcrumb to check that we've reloaded
+    assert.ok(await client.executeScript('return window.__old_joe_was_here = true', []));
+
+    await reload();
+    await wait(500);
+
+    // Wait for the client ui
+    await (await client.$('#client-ui')).waitForExist(10000);
+
+    // Our breadcrumb should be gone now
+    assert.ok(!(await client.executeScript('return window.__old_joe_was_here', [])));
+  });
+};
