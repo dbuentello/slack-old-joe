f8dc9d4c9d1527e0f304894dbd66ae088ff361ff
Felix Rieseberg <felix@felixrieseberg.com>
2019-04-19 17:59:51 -0700

    fix: Re-enable about window test

--- src/smoke/8-child-windows.ts
+++ src/smoke/8-child-windows.ts
@@ -16,45 +16,45 @@ export const test: SuiteMethod = async (client, { it, beforeAll }) => {
     await getBrowserViewHandle(client);
   });
 
-  // it('(Mac) opens about dialog and displays the correct version string', async () => {
-  //   if (process.platform !== 'darwin') return;
+  it('(Mac) opens about dialog and displays the correct version string', async () => {
+    if (process.platform !== 'darwin') return;
 
-  //   // Make sure that we make a blacklist of logs files we won't
-  //   // accept because they already exist
-  //   await openAboutBox();
+    // Make sure that we make a blacklist of logs files we won't
+    // accept because they already exist
+    await openAboutBox();
 
-  //   // What version do we expect?
-  //   await getBrowserViewHandle(client);
+    // What version do we expect?
+    await getBrowserViewHandle(client);
 
-  //   // Returns
-  //   // "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/537.36 (KHTML, like Gecko)
-  //   // AtomShell/3.4.1-beta3179ea19 Chrome/69.0.3497.128 Electron/4.1.3 Safari/537.36 Slack_SSB/3.4.1"
-  //   const userAgent: string = await client.executeScript(
-  //     'return navigator.userAgent',
-  //     []
-  //   );
-  //   const aboutBoxValue: string = await getAboutBoxValue();
-  //   const simpleVersion = userAgent.slice(userAgent.indexOf('Slack_SSB') + 10);
-  //   const fullVersion = userAgent.slice(
-  //     userAgent.indexOf('AtomShell') + 10,
-  //     userAgent.indexOf(' Chrome')
-  //   );
+    // Returns
+    // "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/537.36 (KHTML, like Gecko)
+    // AtomShell/3.4.1-beta3179ea19 Chrome/69.0.3497.128 Electron/4.1.3 Safari/537.36 Slack_SSB/3.4.1"
+    const userAgent: string = await client.executeScript(
+      'return navigator.userAgent',
+      []
+    );
+    const aboutBoxValue: string = await getAboutBoxValue();
+    const simpleVersion = userAgent.slice(userAgent.indexOf('Slack_SSB') + 10);
+    const fullVersion = userAgent.slice(
+      userAgent.indexOf('AtomShell') + 10,
+      userAgent.indexOf(' Chrome')
+    );
 
-  //   console.log(aboutBoxValue, simpleVersion, fullVersion);
+    console.log(aboutBoxValue, simpleVersion, fullVersion);
 
-  //   assert.ok(aboutBoxValue.includes(simpleVersion));
+    assert.ok(aboutBoxValue.includes(simpleVersion));
 
-  //   if (fullVersion.includes('alpha') || fullVersion.includes('beta')) {
-  //     // Covers alpha, beta, etc
-  //     assert.ok(
-  //       aboutBoxValue.toLowerCase().includes('alpha') ||
-  //         aboutBoxValue.includes('beta')
-  //     );
-  //   }
+    if (fullVersion.includes('alpha') || fullVersion.includes('beta')) {
+      // Covers alpha, beta, etc
+      assert.ok(
+        aboutBoxValue.toLowerCase().includes('alpha') ||
+          aboutBoxValue.includes('beta')
+      );
+    }
 
-  //   // Close window
-  //   await closeAboutBox();
-  // });
+    // Close window
+    await closeAboutBox();
+  });
 
   it('creates a post window', async () => {
     // Switch to the posts channel
@@ -64,7 +64,7 @@ export const test: SuiteMethod = async (client, { it, beforeAll }) => {
     await (await client.$('#primary_file_button')).click();
     await wait(100);
     await (await client.$('=Post')).click();
-    await wait(1500);
+    await wait(1000);
 
     // Switch to the new post window
     const { handle } = await getPostWindowHandle(client);
