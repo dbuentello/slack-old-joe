8b6d57ab6254e0335e5249f9b6b4f82a6e719d51
Felix Rieseberg <felix@felixrieseberg.com>
2019-04-26 19:22:04 +0200

    feat: Add preferences tests

--- src/helpers/open-preferences.ts
+++ src/helpers/open-preferences.ts
@@ -1,15 +1,31 @@
-import { sendKeyboardEvent } from './send-keyboard-event';
+import { sendNativeKeyboardEvent } from './send-keyboard-event';
 import { isMac } from './os';
+import { getBrowserViewHandle } from './get-browser-view';
+import { wait } from './wait';
 
 export async function openPreferences(client: BrowserObject) {
-  await sendKeyboardEvent(client, {
+  if (await getIsPreferencesOpen(client)) return;
+
+  await getBrowserViewHandle(client);
+  await sendNativeKeyboardEvent({
     cmd: isMac(),
     ctrl: !isMac(),
-    text: '/'
+    text: ','
   });
 
-  const prefencesModal = await client.$('TODO: MODAL');
+  const prefencesModal = await client.$('.p-prefs_modal');
   await prefencesModal.waitForDisplayed(1000);
 
   return prefencesModal;
+}
+
+export async function closePreferences(client: BrowserObject) {
+  if (!(await getIsPreferencesOpen(client))) return;
+  await sendNativeKeyboardEvent({ text: 'escape' })
+  await wait(500);
+}
+
+export async function getIsPreferencesOpen(client: BrowserObject): Promise<boolean> {
+  const prefencesModal = await client.$('.p-prefs_modal');
+  return prefencesModal.isExisting();
 }
--- src/smoke/preferences.ts
+++ src/smoke/preferences.ts
@@ -3,29 +3,70 @@ import { assert } from 'chai';
 import { SuiteMethod } from '../interfaces';
 import { getBrowserViewHandle } from '../helpers/get-browser-view';
 import { selectNextTeamShortcut } from '../helpers/switch-teams';
-import { openPreferences } from '../helpers/open-preferences';
+import { openPreferences, closePreferences } from '../helpers/open-preferences';
 import { isWin } from '../helpers/os';
+import { wait } from '../helpers/wait';
 
-export const test: SuiteMethod = async (client, { it, beforeAll }) => {
+export const test: SuiteMethod = async (client, { it, afterAll, beforeAll }) => {
   beforeAll(async () => {
     await getBrowserViewHandle(client);
   });
+
+  afterAll(async () => {
+    await closePreferences(client);
+  })
 
   it('can open the preferences', async () => {
     assert.ok(await openPreferences(client), 'could not open preferences');
   });
 
-  it('can disable hardware acceleration', async () => {});
+  it('can disable hardware acceleration', async () => {
+    const advancedButton = await client.$('button=Advanced');
+    await advancedButton.click();
+    await wait(100);
 
-  it('can enable launch on login', async () => {});
+    const disableHwButton = await client.$('strong=Disable hardware acceleration');
+    await disableHwButton.waitForExist(1000);
+    await disableHwButton.click();
+    await wait(200);
 
-  it('can disable launch on login', async () => {});
+    const setting = await client.executeScript('return desktop.app.getPreference("useHwAcceleration")', []);
+    assert.equal(setting, false, 'Setting has not been disabled');
+  });
 
-  it('persists these setting accross teams', async () => {
+  it('persists these setting across teams', async () => {
     await selectNextTeamShortcut(client);
     await openPreferences(client);
 
-    // Todo: Verify that settings are checked
+    const advancedButton = await client.$('button=Advanced');
+    await advancedButton.click();
+    await wait(100);
+
+    const checkboxes = await client.$$('.c-input_checkbox');
+    const disableHwCheckbox = checkboxes[checkboxes.length - 1];
+    assert.ok(await disableHwCheckbox.getAttribute('checked'), 'checkbox is not checked');
+
+    // Switch back
+    await closePreferences(client);
+    await wait(100);
+    await selectNextTeamShortcut(client);
+  });
+
+  it('can enable hardware acceleration', async () => {
+    const disableHwButton = await client.$('strong=Disable hardware acceleration');
+    await disableHwButton.click();
+    await wait(200);
+
+    const setting = await client.executeScript('return desktop.app.getPreference("useHwAcceleration")', []);
+    assert.equal(setting, true, 'Setting has not been enabled');
+  });
+
+  it('can enable launch on login (Windows)', async () => {
+    if (!isWin()) return;
+  });
+
+  it('can disable launch on login (Windows)', async () => {
+    if (!isWin()) return;
   });
 
   it('has Windows notification methods', async () => {
