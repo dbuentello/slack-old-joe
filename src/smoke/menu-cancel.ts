217e5e6da97eaded595d09060b6d546f6a6b4cd8
Felix Rieseberg <felix@felixrieseberg.com>
2019-05-04 19:45:52 +0200

    fix: Retries and cleanup for cancel

--- src/smoke/menu-cancel.ts
+++ src/smoke/menu-cancel.ts
@@ -8,12 +8,20 @@ import { getBrowserViewHandle } from '../helpers/get-browser-view';
 import { clickWindowMenuItem } from '../helpers/click-window-menu-item';
 import { getIsResetAppDataSheetOpen } from '../native-commands/get-reset-app-data-sheet';
 import { getIsNetLogSheetOpen } from '../native-commands/get-restart-net-log-sheet';
+import { focus } from '../native-commands/focus';
 
 export const test: SuiteMethod = async ({ it, beforeAll }) => {
   beforeAll(async () => {
     await getBrowserViewHandle(window.client);
     await switchToChannel(window.client, 'random');
   });
+
+  const retries = 3;
+  const cleanup = async () => {
+    // We'll just mash escape twice 😂
+    await sendNativeKeyboardEvent({ text: 'escape' });
+    await sendNativeKeyboardEvent({ text: 'escape' });
+  }
 
   it('can "cancel" the "Reset App Data" dialog', async () => {
     await clickWindowMenuItem(['Help', 'Troubleshooting', 'Reset App Data…']);
@@ -23,11 +31,16 @@ export const test: SuiteMethod = async ({ it, beforeAll }) => {
     const dialogOpen = await getIsResetAppDataSheetOpen();
     assert.ok(dialogOpen, 'the reset app data dialog (open)');
 
-    await sendNativeKeyboardEvent({ text: 'escape' });
+    await focus();
+    await wait(500);
+    await sendNativeKeyboardEvent({ text: 'escape', noFocus: true });
 
     // Dialog should now be closed
     const dialogClosed = !(await getIsResetAppDataSheetOpen());
     assert.ok(dialogClosed, 'the reset app data dialog (closed)');
+  }, {
+    cleanup,
+    retries
   });
 
   it('can "cancel" the "Restart and Collect Net Logs" dialog', async () => {
@@ -47,5 +60,8 @@ export const test: SuiteMethod = async ({ it, beforeAll }) => {
     // Dialog should now be closed
     const dialogClosed = !(await getIsResetAppDataSheetOpen());
     assert.ok(dialogClosed, 'the restart and collect net logs sheet (closed)');
+  }, {
+    cleanup,
+    retries
   });
 };
