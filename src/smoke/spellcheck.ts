2d39f52d54c2f0c1e7dc8fc6930c0683731d1845
Felix Rieseberg <felix@felixrieseberg.com>
2019-05-06 10:22:12 +0200

    test: Fix spellcheck test on Windows

--- src/smoke/spellcheck.ts
+++ src/smoke/spellcheck.ts
@@ -5,17 +5,25 @@ import { switchToTeam } from '../helpers/switch-teams';
 import { switchToChannel } from '../helpers/switch-channel';
 import { wait } from '../utils/wait';
 import { enterMessage } from '../helpers/enter-message';
-import { sendClickElement } from '../helpers/send-pointer-event';
 import {
   sendKeyboardEvent,
   sendNativeKeyboardEvent
 } from '../helpers/send-keyboard-event';
 import { doTimes } from '../utils/do-times';
 import { clearMessageInput } from '../helpers/clear-message-input';
+import { isWin } from '../utils/os';
+import { setSelection } from '../helpers/set-selection';
+import { reopen } from '../native-commands/reopen';
+import { appState } from '../renderer/state';
+import { openContextMenuForElement } from '../helpers/open-context-menu';
 
 export const test: SuiteMethod = async ({ it, beforeAll }) => {
   beforeAll(async () => {
     await switchToTeam(window.client, 0);
+
+    if (isWin()) {
+      await reopen(appState);
+    }
   });
 
   it(
@@ -27,7 +35,12 @@ export const test: SuiteMethod = async ({ it, beforeAll }) => {
       await focus();
       await wait(1000);
 
-      await sendClickElement(window.client, 'p=mispelled', true);
+      // On Windows, we need to first select the element
+      if (isWin()) {
+        await setSelection(window.client, '#msg_input .ql-editor > p')
+      }
+
+      await openContextMenuForElement(window.client, 'p=mispelled');
       await wait(200);
       await sendNativeKeyboardEvent({ text: 'down', noFocus: true });
       await wait(200);
@@ -36,14 +49,7 @@ export const test: SuiteMethod = async ({ it, beforeAll }) => {
       const messageElement = await window.client.$('p=misspelled');
       await messageElement.waitForExist(1000);
 
-      assert.ok(messageElement, 'text did not get corrected');
-
-      // Hit backspace ten times
-      await doTimes(10, () =>
-        sendKeyboardEvent(window.client, {
-          text: 'Backspace'
-        })
-      );
+      assert.ok(messageElement, 'corrected message elemt');
     },
     {
       cleanup: () => clearMessageInput(window.client),
