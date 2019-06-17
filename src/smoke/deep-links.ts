e077b0c36ca04333db271a13d555d6d0b142a84a
Felix Rieseberg <felix@felixrieseberg.com>
2019-06-17 15:02:38 -0700

    fix: Deep link tests (#16)

--- src/smoke/deep-links.ts
+++ src/smoke/deep-links.ts
@@ -2,19 +2,20 @@ import { assert } from 'chai';
 import { shell } from 'electron';
 
 import { SuiteMethod } from '../interfaces';
-import { getBrowserViewHandle } from '../helpers/get-browser-view';
 import { smokeTeams } from '../smoke-teams';
 import { switchToTeam } from '../helpers/switch-teams';
+import { getSonicWindow } from '../helpers/get-sonic-window';
+import { wait } from '../utils/wait';
 
 export const test: SuiteMethod = async ({ it, beforeAll }) => {
   beforeAll(async () => {
-    await getBrowserViewHandle(window.client);
+    await getSonicWindow(window.client);
   });
 
   it('can open a workspace via deep link', async () => {
     for (const { id, name } of smokeTeams) {
       shell.openExternal(`slack://open?team=${id}`);
-      await getBrowserViewHandle(window.client, 300);
+      await wait(1000);
       assert.include(await window.client.getTitle(), name);
     }
   });
