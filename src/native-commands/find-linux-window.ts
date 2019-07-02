7d3476220354a2373f2519a04b07052432660d63
Carlos Valdez <cvaldez@slack-corp.com>
2019-07-02 16:02:09 -0700

    using shelljs to do command on linux

--- src/native-commands/find-linux-window.ts
+++ src/native-commands/find-linux-window.ts
@@ -1,13 +1,14 @@
-import { sh } from 'run-sh'
+const shell = require('shelljs');
 
 export async function findWindow(name: string): Promise<any> {
   // const stringOfWindows = await sh("wmctrl -l");
-  sh("wmctrl -l").then(function(res: { stdout: any; stderr: any; }) {
-    console.log("Output from Linux command wmctrl -l : ", res.stdout);
-    console.log(`Does output contain ${name}? ${res.stdout.contains(name)}`);
-  }, function(err: any) {
-    console.log("Error running `times`: " + err);
-  });
+  console.log(String(shell.exec('wmctrl -l')));
+  // sh("wmctrl -l").then(function(res: { stdout: any; stderr: any; }) {
+  //   console.log("Output from Linux command wmctrl -l : ", res.stdout);
+  //   console.log(`Does output contain ${name}? ${res.stdout.contains(name)}`);
+  // }, function(err: any) {
+  //   console.log("Error running `times`: " + err);
+  // });
   // console.log(stringOfWindows);
   // filter for string given .contains? (in this case will be reset slack?)
   // if present, return true. 
