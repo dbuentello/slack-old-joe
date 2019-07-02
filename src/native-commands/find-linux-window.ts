55b3d8e9ec03fe4c233956fdd287ef403c0b7203
Carlos Valdez <cvaldez@slack-corp.com>
2019-07-02 16:41:12 -0700

    clean code

--- src/native-commands/find-linux-window.ts
+++ src/native-commands/find-linux-window.ts
@@ -1,18 +1,9 @@
-const shell = require('shelljs');
+import shell = require('shelljs');
+// import { shell } from 'shelljs'
 
-export async function findWindow(name: string): Promise<any> {
-  // const stringOfWindows = await sh("wmctrl -l");
+export async function findWindow(name: string): Promise<Boolean> {
+  // set the execPath 
   shell.config.execPath = String(shell.which('node'));
-  console.log(shell.exec('wmctrl -l').stdout);
-  const s:string = shell.exec('wmctrl -l');
-  return s.includes(name);
-  // sh("wmctrl -l").then(function(res: { stdout: any; stderr: any; }) {
-  //   console.log("Output from Linux command wmctrl -l : ", res.stdout);
-  //   console.log(`Does output contain ${name}? ${res.stdout.contains(name)}`);
-  // }, function(err: any) {
-  //   console.log("Error running `times`: " + err);
-  // });
-  // console.log(stringOfWindows);
-  // filter for string given .contains? (in this case will be reset slack?)
-  // if present, return true. 
+  // does the list of windows contain what we are looking for? 
+  return (shell.exec('wmctrl -l')).includes(name);
 }
