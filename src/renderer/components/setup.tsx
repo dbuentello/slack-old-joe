d496ea17cb462747ed2db5b42ac931862bd61b3a
Carlos Valdez <carlosvr_24@outlook.com>
2019-07-23 10:42:49 -0700

    add remember my choice

--- src/renderer/components/setup.tsx
+++ src/renderer/components/setup.tsx
@@ -48,6 +48,10 @@ export class Setup extends React.Component<SetupProps, {}> {
           <ul>
             <li>Make sure your browser is signed into the teams.</li>
             <li>
+              When you first do a sign in test, chrome will ask you to Open Slack,
+              click on 'Remember my choice' to do this automatically.
+            </li>
+            <li>
               Ensure that you're running a version of Old Joe that matches the
               Slack app's Electron version. It should be v{DRIVER_VERSION}.x.
             </li>
