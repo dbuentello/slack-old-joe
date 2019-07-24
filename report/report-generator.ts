d0a2e3ca686b16d234030b34bff0f5048afeb4de
Carlos Valdez <carlosvr_24@outlook.com>
2019-07-24 14:15:40 -0700

    add base file that old-joe will use to make the report

--- report/report-generator.ts
+++ report/report-generator.ts
@@ -1,2 +1,4 @@
 // here we'll get the report as a string then the code that calls this funciton will write to file. 
 
+// fetches file 
+const ojr = /** @type {OJ.Result} */ (require('../../lighthouse-core/test/results/sample_v2.json'));
