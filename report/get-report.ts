fc04cb7ce8e5a0bdc23d57e5316430bad29478ec
Carlos Valdez <carlosvr_24@outlook.com>
2019-07-24 15:13:49 -0700

    adding the page code

--- report/get-report.ts
+++
@@ -1,3 +0,0 @@
-// here we'll get the report as a string then the code that calls this funciton will write to file. 
-import * as fs from 'fs-extra';
-
--- report/report-generator.ts
+++ report/report-generator.ts
@@ -1,4 +1,5 @@
 // here we'll get the report as a string then the code that calls this funciton will write to file. 
+import * as fs from 'fs-extra';
 
 // fetches file 
 const ojr = /** @type {OJ.Result} */ (require('../../lighthouse-core/test/results/sample_v2.json'));
