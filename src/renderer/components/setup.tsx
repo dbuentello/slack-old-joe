016dafa78b7c7fed0e8f3422a546c23b662e5b42
Felix Rieseberg <felix@felixrieseberg.com>
2019-11-19 15:24:54 -0800

    fix: Clearer instructions

--- src/renderer/components/setup.tsx
+++ src/renderer/components/setup.tsx
@@ -52,11 +52,6 @@ export class Setup extends React.Component<SetupProps, {}> {
               Slack, click on 'Remember my choice' to do this automatically.
             </li>
             <li>
-              If a test fails, make sure to retry the suite on its own.
-              Sometimes there is an ECONNREFUSED error that causes Old Joe to
-              fail.
-            </li>
-            <li>
               Ensure that you're running a version of Old Joe that matches the
               Slack app's Electron version. It should be v{DRIVER_VERSION}.x.
             </li>
