d658f31d41886768b23dbbc04bd5393529e9bef3
Andy Locascio <andy@slack-corp.com>
2020-01-15 13:53:50 -0800

    feat: allow fuzziness in switchToChannel
    This works around the fact that when a channel or DM is badged, the inner text of the link becomes Jane Doe Two<CR>1.

--- src/helpers/switch-channel.ts
+++ src/helpers/switch-channel.ts
@@ -1,8 +1,12 @@
 import { BrowserObject } from 'webdriverio';
 import { wait } from '../utils/wait';
 
-export async function switchToChannel(client: BrowserObject, channel: string) {
-  const channelLink = await client.$(`=${channel}`);
+export async function switchToChannel(
+  client: BrowserObject,
+  channel: string,
+  { fuzzy = false }: { fuzzy?: boolean } = {}
+) {
+  const channelLink = await client.$(`${fuzzy ? '*' : ''}=${channel}`);
   await channelLink.waitForExist(1500);
   await channelLink.moveTo();
   await channelLink.click();
