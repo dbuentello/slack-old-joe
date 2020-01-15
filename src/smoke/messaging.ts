a47298e0d03d8ac358d7df0d96671ef8b5ffd5b1
Andy Locascio <andy@slack-corp.com>
2020-01-15 13:54:33 -0800

    feat: add DM badge test

--- src/smoke/messaging.ts
+++ src/smoke/messaging.ts
@@ -4,6 +4,7 @@ import { SuiteMethod, JoeBrowserObject } from '../interfaces';
 import { switchToChannel } from '../helpers/switch-channel';
 import { enterMessage } from '../helpers/enter-message';
 import { getSonicWindow } from '../helpers/get-sonic-window';
+import { switchToTeam } from '../helpers/switch-teams';
 
 async function assertVideo(client: JoeBrowserObject) {
   // Play the video
@@ -72,5 +73,38 @@ export const test: SuiteMethod = async ({ it, beforeAll }) => {
     assert.ok(await randomDesc.waitForExist(1000));
 
     await assertVideo(window.client);
+  });
+
+  const JANE_DOE_DM_ID = 'DSP5N4THC';
+  it('DM badge updates when reading incoming DM', async () => {
+    const testValue = Date.now().toString();
+
+    await switchToChannel(window.client, 'random');
+
+    // Switch to Old Joe 2 and send a message to Jane Doe One.
+    await switchToTeam(1);
+    await switchToChannel(window.client, 'Jane Doe One', { fuzzy: true });
+    await enterMessage(window.client, testValue, true);
+
+    // Switch back to Old Joe 1
+    await switchToTeam(0);
+
+    // Check to make sure the DM with Jane Doe Two is badged.
+    const sidebarDmListItem = await window.client.$(
+      `[data-qa-channel-sidebar-channel-id="${JANE_DOE_DM_ID}"]`
+    );
+    const dmMentionBadge = await sidebarDmListItem.$(
+      '[data-qa="mention_badge"]'
+    );
+    await dmMentionBadge.waitForExist(1000);
+
+    await switchToChannel(window.client, 'Jane Doe Two', { fuzzy: true });
+
+    // Verify message receive works.
+    const postedMessage = await window.client.$(`div=${testValue}`);
+    await postedMessage.waitForExist(1500);
+
+    // Check that the DM badge has disappeared.
+    await dmMentionBadge.waitForExist(1000, true);
   });
 };
